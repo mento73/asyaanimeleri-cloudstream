494ddbdc8c37e6372badfe20fd1b9103adf57d36
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-23 21:06:50 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,10 +68,10 @@ subprojects {
     }
 
     dependencies {
-        val cloudstream by configurations
+       
         val implementation by configurations
 
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+        
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
