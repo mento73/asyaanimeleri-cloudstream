3e42871304cf85b75e2e90235859ca8afe9594df
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-23 21:35:51 +0300

    Update AsyaAnimeleriPlugin.kt

--- AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleriPlugin.kt
+++ AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleriPlugin.kt
@@ -1,12 +1,11 @@
 package com.example
 
-import android.content.Context
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AsyaAnimeleriPlugin : Plugin() {
-    override fun load(context: Context) {
+class AsyaAnimeleriPlugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(AsyaAnimeleriProvider())
     }
 }
