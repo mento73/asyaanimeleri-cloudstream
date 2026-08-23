da0019d62828658b1f461f5e7a1bc9d0ee281b2e
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-23 19:48:28 +0300

    Update AsyaAnimeleriProvider.kt

--- AsyaAnimeleri/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleriProvider.kt
+++ AsyaAnimeleri/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleriProvider.kt
@@ -1,7 +1,13 @@
 package com.example
 
+import com.lagradost.cloudstream3.HomePageList
+import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newAnimeSearchResponse
 
 class AsyaAnimeleriProvider : MainAPI() {
 
@@ -14,4 +20,46 @@ class AsyaAnimeleriProvider : MainAPI() {
         TvType.Anime,
         TvType.AnimeMovie
     )
+
+    override val mainPage = mainPageOf(
+        "$mainUrl/series/list-mode" to "Animeler"
+    )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+
+        val document = app.get(request.data).document
+
+        val animeList = document
+            .select("a")
+            .mapNotNull { element ->
+
+                val title = element.text().trim()
+                val url = element.absUrl("href")
+
+                if (
+                    title.isBlank() ||
+                    url.isBlank() ||
+                    !url.contains(mainUrl)
+                ) {
+                    null
+                } else {
+                    newAnimeSearchResponse(
+                        title,
+                        url,
+                        TvType.Anime
+                    )
+                }
+            }
+            .distinctBy { it.url }
+
+        return newHomePageResponse(
+            HomePageList(
+                request.name,
+                animeList
+            )
+        )
+    }
 }
