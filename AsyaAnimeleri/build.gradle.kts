8112b1b86c70be7bccdc00b039668440becccb86
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-23 19:40:37 +0300

    Create build.gradle.kts

---
+++ AsyaAnimeleri/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "AsyaAnimeleri CloudStream eklentisi"
+    authors = listOf("Damla")
+    status = 1
+    tvTypes = listOf("Anime")
+    language = "tr"
+}
