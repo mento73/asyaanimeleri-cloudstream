85a78b947faf21c9f4d3def0be8f03aa0ffe2f01
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-23 19:50:06 +0300

    Update AsyaAnimeleriProvider.kt

--- AsyaAnimeleri/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleriProvider.kt
+++ AsyaAnimeleri/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleri/src/main/kotlin/com/example/AsyaAnimeleriProvider.kt
@@ -1,19 +1,13 @@
 package com.example
 
-import com.lagradost.cloudstream3.HomePageList
-import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.newHomePageResponse
-import com.lagradost.cloudstream3.newAnimeSearchResponse
 
 class AsyaAnimeleriProvider : MainAPI() {
 
     override var mainUrl = "https://asyaanimeleri.top"
     override var name = "AsyaAnimeleri"
-    override val hasMainPage = true
     override var lang = "tr"
 
     override val supportedTypes = setOf(
@@ -21,45 +15,9 @@ class AsyaAnimeleriProvider : MainAPI() {
         TvType.AnimeMovie
     )
 
-    override val mainPage = mainPageOf(
-        "$mainUrl/series/list-mode" to "Animeler"
-    )
+    override val hasMainPage = false
 
-    override suspend fun getMainPage(
-        page: Int,
-        request: MainPageRequest
-    ): HomePageResponse {
-
-        val document = app.get(request.data).document
-
-        val animeList = document
-            .select("a")
-            .mapNotNull { element ->
-
-                val title = element.text().trim()
-                val url = element.absUrl("href")
-
-                if (
-                    title.isBlank() ||
-                    url.isBlank() ||
-                    !url.contains(mainUrl)
-                ) {
-                    null
-                } else {
-                    newAnimeSearchResponse(
-                        title,
-                        url,
-                        TvType.Anime
-                    )
-                }
-            }
-            .distinctBy { it.url }
-
-        return newHomePageResponse(
-            HomePageList(
-                request.name,
-                animeList
-            )
-        )
+    override suspend fun search(query: String): List<SearchResponse> {
+        return emptyList()
     }
 }
