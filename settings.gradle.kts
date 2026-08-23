5969fb85e74d08386156068af322b040c9f9cbda
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-23 19:32:10 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+rootProject.name = "AsyaAnimeleriCloudstream"
 
+val disabled = listOf<String>()
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
+    }
+}
+
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
