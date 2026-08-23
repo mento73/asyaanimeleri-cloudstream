fc567400a48eccdc60419be312769424068bbbf8
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-23 21:24:42 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,15 +68,15 @@ subprojects {
     }
 
     dependencies {
-       
-        val implementation by configurations
+    val cloudstream by configurations
+    val implementation by configurations
 
-        
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-    }
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+}
 }
 
 task<Delete>("clean") {
