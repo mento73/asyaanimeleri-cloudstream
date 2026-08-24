0e410edd978391277abde825a30807578bd768e2
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-24 21:39:05 +0300

    Update DiziGomPlugin.kt

--- DiziGom/src/main/kotlin/com/anihepsi/dizigom/DiziGomPlugin.kt
+++ DiziGom/src/main/kotlin/com/anihepsi/dizigom/DiziGomPlugin.kt
@@ -3,6 +3,14 @@ package com.anihepsi.dizigom
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
+/*
+ * Implementation references:
+ * Kekik / Kraptor ecosystem
+ * ahadeniz / nik-cloudstream
+ *
+ * Modernized for Anihepsi.
+ */
+
 @CloudstreamPlugin
 class DiziGomPlugin : BasePlugin() {
 
