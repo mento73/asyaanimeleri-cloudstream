fd53fe06905dcb9769b552b6daef2435c37c342b
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-24 12:07:30 +0300

    Create build.gradle.kts

---
+++ AnimeciX/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "AnimeciX CloudStream extension"
+    authors = listOf(
+        "keyiflerolsun",
+        "inatchii",
+        "JustRelaxable"
+    )
+    status = 1
+    tvTypes = listOf("Anime")
+    language = "tr"
+    iconUrl = "https://anm.cx/favicon.ico"
+}
