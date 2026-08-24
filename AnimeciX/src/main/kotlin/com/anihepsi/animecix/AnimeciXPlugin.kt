5972f1ee0c1dfe2ef00fc57a2a453a2633dde6c7
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-24 12:11:55 +0300

    Create AnimeciXPlugin.kt

---
+++ AnimeciX/src/main/kotlin/com/anihepsi/animecix/AnimeciXPlugin.kt
@@ -0,0 +1,13 @@
+package com.anihepsi.animecix
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class AnimeciXPlugin : BasePlugin() {
+
+    override fun load() {
+        registerMainAPI(AnimeciXProvider())
+        registerExtractorAPI(TauVideo())
+    }
+}
