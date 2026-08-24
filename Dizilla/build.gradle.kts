7b2b5f3c00846b1cd91d4a2ca61a01e352d827a6
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-24 22:57:16 +0300

    Create build.gradle.kts

---
+++ Dizilla/build.gradle.kts
@@ -0,0 +1,22 @@
+version = 1
+
+cloudstream {
+    authors = listOf(
+        "keyiflerolsun",
+        "nikyokki"
+    )
+
+    language = "tr"
+
+    description =
+        "Dizilla yabancı dizileri Türkçe altyazı ve dublaj seçenekleriyle sunan CloudStream eklentisi."
+
+    status = 1
+
+    tvTypes = listOf(
+        "TvSeries"
+    )
+
+    iconUrl =
+        "https://www.google.com/s2/favicons?domain=dizilla40.com&sz=%size%"
+}
