33f9edbf34409226a2fadd8918b331c298ae81bf
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-24 15:07:55 +0300

    Create build.gradle.kts

---
+++ DiziPal/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "DiziPal CloudStream extension"
+    authors = listOf(
+        "keyiflerolsun"
+    )
+    status = 1
+    tvTypes = listOf("TvSeries")
+    language = "tr"
+}
