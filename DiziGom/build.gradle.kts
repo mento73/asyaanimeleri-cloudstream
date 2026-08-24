67526f5d4b7b9d2032f80da708ab96362b09093d
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-24 16:20:14 +0300

    Create build.gradle.kts

---
+++ DiziGom/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "DiziGom CloudStream extension"
+    authors = listOf(
+        "keyiflerolsun"
+    )
+    status = 1
+    tvTypes = listOf("TvSeries")
+    language = "tr"
+}
