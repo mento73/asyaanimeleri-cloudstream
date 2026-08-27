146d66cc1373ee177e1e355794ea9959069e21fa
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-27 16:19:56 +0300

    Create build.gradle.kts

---
+++ DiziBox/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    authors = listOf("keyiflerolsun", "nikyokki")
+    description = "DiziBox yabancı dizi kaynağı."
+    status = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizibox.live&sz=%size%"
+}
