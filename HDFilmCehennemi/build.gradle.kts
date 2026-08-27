de6ca6db591326592f75f79257bf412e501b7add
mento73 <87208919+mento73@users.noreply.github.com>
2026-08-27 12:18:16 +0300

    Create build.gradle.kts

---
+++ HDFilmCehennemi/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+
+    authors = listOf("Hexated")
+
+    description = "HDFilmCehennemi film ve yabancı dizi kaynağı."
+
+    status = 1
+
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie"
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.nl&sz=%size%"
+}
